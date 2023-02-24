331d2a6e1433fdb154f165ffb6003920847b4ce6
samroodAli <devsamrood@gmail.com>
2023-02-24 22:14:36 +0530

    feat: refactor and organise files and file names to be output folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     rollupOptions: {
+      input: {
+        "pop-up": "pop-up.html",
+        content: "src/content.ts",
+        // background: "src/background.ts",
+      },
       output: {
         entryFileNames: `[name].js`,
         assetFileNames: `[name].[ext]`,
