c2018e950d783d616c3ca7f4cb42c97c30a2c01a
samroodAli <devsamrood@gmail.com>
2023-02-25 12:00:38 +0530

    refactor: remove unused config in vite config options

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        css: "./src/extension.css"
-      }
-    }
-  },
   plugins: [crx({ manifest })]
 });
