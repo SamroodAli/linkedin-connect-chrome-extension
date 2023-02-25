211924e471ae9616a11302cfca6cd30bfb8f460b
samroodAli <devsamrood@gmail.com>
2023-02-25 20:56:01 +0530

    fix: change to while loop and wait for one button to resolve

--- src/content-scripts/index.ts
+++ src/content-scripts/index.ts
@@ -12,17 +12,21 @@ async function main() {
 async function onConnectBtnClick(data: OnConnectClickPayload) {
   const allConnectElements = document.querySelectorAll("button");
 
-  console.log(data.isDemo);
-
-  let timeout = 0;
-
-  allConnectElements.forEach((button) => {
+  let isStarted = false;
+  for (let i = 0; i < allConnectElements.length; i++) {
+    const button = allConnectElements[i];
     const buttonIntent = button.getAttribute("aria-label");
+
     if (buttonIntent?.includes("Invite")) {
-      connectButtonClick(button, timeout, data.isDemo);
-      timeout += randomIntFromInterval(3000, 5000);
+      await connectButtonClick(
+        button,
+        !isStarted ? 0 : randomIntFromInterval(3000, 5000),
+        data.isDemo
+      );
+
+      isStarted = true;
     }
-  });
+  }
 }
 
 async function connectButtonClick(
@@ -30,17 +34,17 @@ async function connectButtonClick(
   timeout: number,
   isDemo: boolean
 ) {
-  new Promise((resolve) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       if (isDemo) {
         button.style.backgroundColor = "red";
       } else {
         button.click();
+        //TODO: check if there is a pop-up before resolving this
       }
       resolve(button);
     }, timeout);
   });
-  // mocking button click
 }
 
 function randomIntFromInterval(min: number, max: number) {
